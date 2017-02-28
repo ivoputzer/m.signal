7a67b64043b6c03e5278a309e0b96246502447a5
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2017-02-28 23:51:05 +0100

    removes obsolete comments from index

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const {createServer} = require('wss')
 
 module.exports = createServer(function (ws) {
-  // const {pathname} = ws.pathname = parse(ws.upgradeReq.url).pathname
-  // const {isOwner} = ws.isOwner = Array.from(this.clients).filter(by({pathname}))
-  // console.log(ws.upgradeReq)
   const [owner] = this.clients
   return ws !== owner && ws.once('message', (data) => {
     owner.send(data)
     owner.once('message', (data) => ws.send(data))
   })
-  // function neighbors (ws, {clients}) {
-  //   const pathname =
-  //   return Array.from(clients).filter(ws => {
-  //     return pathname === parse(ws.upgradeReq.url).pathname
-  //   })
-  // }
 })
